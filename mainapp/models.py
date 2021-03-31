fdd4a955c9cdf98b1666fb7a8930825177f22557
AlexeiRos <rostokin.s@mail.ru>
2021-03-31 13:25:39 +0300

    Testing

--- mainapp/models.py
+++ mainapp/models.py
@@ -151,7 +151,7 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
 
-    owner = models.ForeignKey('Customer', null=True, blank=True, verbose_name='Владелец', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Общая цена', null=True, blank=True)
